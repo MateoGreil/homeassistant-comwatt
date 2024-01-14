362d2b978235092ad56c86bcf6bc6ea621e5bb05
Mateo Greil <mat@greil.fr>
2024-01-14 20:39:18 +0100

    :construction: WIP

--- custom_components/comwatt/client.py
+++ custom_components/comwatt/client.py
@@ -1,3 +1,13 @@
+"""Client for Comwatt and Comwatt Legacy."""
 from comwatt_client import ComwattClient
 
-comwatt_client = ComwattClient()
+class Client:
+    def __init__(self, api = "energy"):
+        self.api = api
+        if self.api == "energy":
+            self.client = ComwattClient()
+        else:
+            self.client = ComwattClientLegacy()
+
+
+comwatt_client = Client()
